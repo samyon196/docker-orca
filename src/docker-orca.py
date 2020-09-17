c1dca1c8e1751540c4ae10592df3931eaa5fe472
samyon196 <samyon.v@gmail.com>
2020-09-17 17:33:44 +0300

    Added yaml read

--- src/docker-orca.py
+++ src/docker-orca.py
@@ -1,12 +1,26 @@
 import sys
 import yaml
 
+def read_configuration(filename):
+    with open(filename, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def system_up():
+    print("starting")
+    print("config is " + str(read_configuration(sys.argv[2])))
+
+def system_down():
+    print("stopping")
+
 # Run using: docker-orca.py <start/stop> <
 if __name__ == "__main__":
     opname = sys.argv[1] 
-    if opname == 'start':
-        print('starting')
-    elif opname == 'stop':
-        print('stopping')
+    if opname == 'up':
+        system_up()
+    elif opname == 'down':
+        system_down()
     else:
         print('wrong use')
